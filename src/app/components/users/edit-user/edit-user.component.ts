a35ef0262057c427c3ec321be988c922dd145056
Mykon89 <helvis89@gmail.com>
2023-12-29 17:58:55 -0300

    edit-user - criando o updateUser

--- src/app/components/users/edit-user/edit-user.component.ts
+++ src/app/components/users/edit-user/edit-user.component.ts
@@ -10,6 +10,10 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./edit-user.component.scss']
 })
 export class EditUserComponent {
+
+  formData: {id_user: string; name: string; login: string; password: string; active: number; contact: string;
+     usergroup: number} = {id_user: this.identity, name: '', login: '', password: '', active: 1, contact: '', usergroup: 1};
+
   ngOnInit(): void {
     this.getUserById();
   }
@@ -18,23 +22,20 @@ export class EditUserComponent {
     return localStorage.getItem('identifier') || ''; // Obter o nome do usuário do localStorage
   }
 
-
   constructor(private api: UserService, public globalService: GlobalService, public dialogRef: MatDialogRef<DialogFormDetailsComponent>) {}
 
 // objeto para receber os dados da api
   user: any[] = [];
   teste: any;
 
+
   isLoad: boolean = false;
    // Método para filtro e paginação
-   getUserById() {
+  getUserById() {
     this.isLoad = true;
-    this.api
-      .getUserById(this.identity)
-      .then(
-        (response) => {
+    this.api.getUserById(this.identity).then((response) => {
           this.user = response.data; // Armazene os usuários na variável 'usuarios'
-          console.log(this.user);
+          // daí a gente faria assim..
           this.isLoad = false;
           if (response.status === true) {
           }else{
@@ -45,8 +46,21 @@ export class EditUserComponent {
         (error: any) => {
           // this.isLoad = false;
           console.error('Erro ao buscar usuários:', error);
-        }
-      );
+        })
+  }
+
+  updateUser() {
+    const {id_user, name, login, password, active, contact, usergroup} = this.formData;
+    this.api.updateUser(id_user, name, login, password, active, contact, usergroup).then((response) => {this.user = response.data;
+      if (response.status === true) {
+        this.globalService.openSnackBar('Registro alterado com sucesso', 'Ok',  'Sucesso!', 'success-snackbar');
+      } else {
+        this.globalService.openSnackBar('Preencha todos os campos', 'Ok',  'Erro!', 'error-snackbar');
+        this.isLoad = false;
+      }},
+      (error: any) => {
+        console.error('Erro ao alterar usuário: ', error);
+      })
   }
 
   msg(){
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,74 +8,34 @@ export class UserService {
   private apiUrl = 'https://gym-dev.com/lanchonete/';
 
   constructor(private http: HttpClient) {}
+
   // .getUsers(name, login, active, pagination, perPage)
   getUsers( user: string, login: string, active: number, nextToPage: number, perPage: number): Promise<any> {
-    const formData = {
-      name: user,
-      login: login,
-      active: active,
-      page: nextToPage,
-      per_page: perPage,
-    };
+    const formData = {name: user, login: login, active: active, page: nextToPage, per_page: perPage};
     const endPoint = `${this.apiUrl}/user/users/`;
-    return this.http
-      .post(endPoint, formData)
-      .toPromise()
-      .then((response) => {
-        // Resetar temporizador de inatividade após obter usuários
-
-        return response as any[];
-      })
-      .catch((error) => {
-        return Promise.reject(error);
-      });
+    return this.http.post(endPoint, formData).toPromise().then((response) => {return response as any[]}).catch((error) => {
+        return Promise.reject(error)});
   }
 
-
-  insertUser(
-    name: string,
-    login: string,
-    password: string,
-    active: number,
-    contact: string,
-    usergroup: number,
-  ): Promise<any> {
-    const formData = {
-      name: name,
-      login: login,
-      password: password,
-      active: active,
-      contact: contact,
-      usergroup: usergroup,
-    };
+  insertUser(name: string, login: string, password: string, active: number, contact: string, usergroup: number,): Promise<any> {
+    const formData = {name: name, login: login, password: password, active: active, contact: contact, usergroup: usergroup,};
     const endPoint = `${this.apiUrl}/user/create/`;
-    return this.http
-      .post(endPoint, formData)
-      .toPromise()
-      .then((response) => {
-        // Resetar temporizador de inatividade após obter usuários
-
-        return response as any[];
-      })
-      .catch((error) => {
-        return Promise.reject(error);
-      });
+    return this.http.post(endPoint, formData).toPromise().then((response) => {return response as any[];}).catch((error) => {
+        return Promise.reject(error)});
   }
 
   getUserById( id_user: string): Promise<any> {
-    const formData = {
-      id_user: id_user,
-    };
+    const formData = {id_user: id_user};
     const endPoint = `${this.apiUrl}/user/users/`;
-    return this.http
-      .post(endPoint, formData)
-      .toPromise()
-      .then((response) => {
-        return response as any[];
-      })
-      .catch((error) => {
+    return this.http.post(endPoint, formData).toPromise().then((response) => {return response as any[]}).catch((error) => {
         return Promise.reject(error);
       });
+  }
+
+  updateUser(id_user: string, name: string, login: string, password: string, active: number, contact: string, usergroup: number, ): Promise<any> {
+    const formData = {id_user: id_user, name: name, login: login, password: password, active: active, contact: contact, usergroup: usergroup, };
+    const endPoint = `${this.apiUrl}/user/update/`;
+    return this.http.post(endPoint, formData).toPromise().then((response) => {return response as any[];}).catch((error) => {return Promise.reject(error)})
   }
 
 }
