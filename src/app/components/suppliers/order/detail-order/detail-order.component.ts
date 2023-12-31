5374fdb4f02066eb1ff7cdae56948807fd8d8f43
luiz <luizfelipetomaz30@gmail.com>
2023-12-31 13:41:01 -0300

    detail-order.service + html + css - adicionando detalhes do pedido no card

--- src/app/components/suppliers/order/detail-order/detail-order.component.ts
+++ src/app/components/suppliers/order/detail-order/detail-order.component.ts
@@ -33,24 +33,37 @@ export class DetailOrderComponent {
 
   supplier: string = '';
 
+  total: number = 0;
+
   getDetailOrder(pagination: number) {
-    this.isLoad = true;
+    this.isLoad = true; // variavel que controla o simbolo de loading
     this.api.getDetailOrder(this.identity, pagination, this.perPage).then((response) => {
-    this.detailOder = response.data; 
-    this.totalPages = response.total_pages;
-    this.currentPage = response.current_page;
-    // this.supplier = response.data[0].
-    // daí a gente faria assim..
-    this.isLoad = false;
-    if (response.status === true) {
-    }else{
-      // this.globalService.openSnackBar('Nenhum registro encontrado', 'Ok',  'Erro!', 'error-snackbar');
+      this.detailOder = response.data; 
+      this.totalPages = response.total_pages;
+      this.currentPage = response.current_page;
+      this.supplier = response.data[0].supplier;
+
+      // bloco responsavel por fazer a soma entre os subtotais, para nao precisar criar outra chamada para api
+      this.total = this.sumSubTotal();
+      // fim do bloco de somar valores
+      
+      // daí a gente faria assim..
       this.isLoad = false;
-  }
+      if (response.status === true) {
+      }else{
+        // this.globalService.openSnackBar('Nenhum registro encontrado', 'Ok',  'Erro!', 'error-snackbar');
+        this.isLoad = false;
+    }
   },
   (error: any) => {
     // this.isLoad = false;
     console.error('Erro ao buscar usuários:', error);
   })
+  }
+
+
+  sumSubTotal(): number {
+    return this.detailOder.reduce((acumulador, objeto) => acumulador + objeto.subtotal, 0);
+    // console.log(acumulador);
   }
 }
