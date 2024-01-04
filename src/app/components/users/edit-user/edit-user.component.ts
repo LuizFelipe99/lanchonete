e26610a2b908ed926f4804376e7061e8c548dc68
luiz <luizfelipetomaz30@gmail.com>
2024-01-04 20:07:50 -0300

    ajustando edit-user

--- src/app/components/users/edit-user/edit-user.component.ts
+++ src/app/components/users/edit-user/edit-user.component.ts
@@ -24,21 +24,12 @@ export class EditUserComponent {
   constructor(private api: UserService, public globalService: GlobalService, public dialogRef: MatDialogRef<DialogFormDetailsComponent>) {
   }
 
-
-
   users: User[];
   filterUser: UserFilter = { id_user: this.identity};
   newUser: User = {name: '', login: '', password: '', active: 0, contact: '', usergroup: 0};
-  // variaveis para controlar paginação
-  page: number = 1;
-  totalUsers: number = 0;
-  totalPages: number = 0;
-  currentPage: number = 1;
   isLoad: boolean = false;
 
-
-// passando a pagina por parametro para a paginação
-// a pagina pode variar de acordo com o botao do form de paginar, ele sempre incrementa/decrementa current_page + 1 ou -1 depende da ação
+// função que chama o usuario clicado enviando somente o id
 getUserById(){
     this.isLoad = true;
     this.api.getUserById(this.filterUser).subscribe(data => {
@@ -47,15 +38,18 @@ getUserById(){
         this.isLoad = false;
       }else{
         this.users = data.data;
-        this.totalPages = data.total_pages;
-        this.currentPage = data.current_page;
-        this.totalUsers = data.users;
+        this.newUser.name = this.users[0].name;
+        this.newUser.login = this.users[0].login;
+        this.newUser.password = this.users[0].password;
+        this.newUser.active = this.users[0].active;
+        this.newUser.contact = this.users[0].contact;
+        this.newUser.usergroup = this.users[0].usergroup;
         this.isLoad = false;
       }
     });
   }
-
-
+  
+// função que atualiza o cadastro
   updateUser(){
     console.log(this.newUser);
     this.isLoad = true;
@@ -72,79 +66,8 @@ getUserById(){
     );
   }
   
-  // updateUserr() {
-  //   // const {id_user, name, login, password, active, contact, usergroup} = this.formData;
-  //   this.api.updateUser(id_user, name, login, password, active, contact, usergroup).then((response) => {this.user = response.data;
-  //     if (response.status === true) {
-  //       this.globalService.openSnackBar('Registro alterado com sucesso', 'Ok',  'Sucesso!', 'success-snackbar');
-  //       this.getUserById();
-  //     } else {
-  //       this.globalService.openSnackBar('Preencha todos os campos', 'Ok',  'Erro!', 'error-snackbar');
-  //       this.isLoad = false;
-  //     }},
-  //     (error: any) => {
-  //       console.error('Erro ao alterar usuário: ', error);
-  //     })
-  // }
-
   cancel(): void {
     this.dialogRef.close();
   }
-
-
-
-  formData: {
-      id_user: string;
-      name: string;
-      login: string;
-      password: string;
-      active: number;
-      contact: string;
-      usergroup: number
-    } =
-    {
-      id_user: this.identity,
-      name: '',
-      login: '',
-      password: '',
-      active: 1,
-      contact: '',
-      usergroup: 1
-    };
-
-  // // objeto para receber os dados da api
-  user: any[] = [];
-  // isLoad: boolean = false;
-
-  //  // Método para filtro e paginação
-  // getUserById() {
-  //   this.isLoad = true;
-  //   this.api.getUserById(this.identity).then((response) => {
-  //         this.user = response.data; // Armazene os usuários na variável 'usuarios'
-  //         this.formData.name = response.data[0].name;
-  //         this.formData.login = response.data[0].login;
-  //         this.formData.password = response.data[0].password;
-  //         this.formData.active = response.data[0].active;
-  //         this.formData.contact = response.data[0].contact;
-  //         this.formData.usergroup = response.data[0].usergroup;
-  //         // daí a gente faria assim..
-  //         this.isLoad = false;
-  //         if (response.status === true) {
-  //         }else{
-  //           // this.globalService.openSnackBar('Nenhum registro encontrado', 'Ok',  'Erro!', 'error-snackbar');
-  //           this.isLoad = false;
-  //       }
-  //       },
-  //       (error: any) => {
-  //         // this.isLoad = false;
-  //         console.error('Erro ao buscar usuários:', error);
-  //       })
-  // }
-
-
-
-
-
-  
 
 }
--- src/app/models/User/user.model.ts
+++ src/app/models/User/user.model.ts
@@ -1,11 +1,11 @@
 export interface User{
   id_user?: string;
-  name: string;
-  login: string;
-  password: string;
-  active: number;
-  contact: string;
-  usergroup: number;
+  name?: string;
+  login?: string;
+  password?: string;
+  active?: number;
+  contact?: string;
+  usergroup?: number;
   dt_created?: string;
 }
 
